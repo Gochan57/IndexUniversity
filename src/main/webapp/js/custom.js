e60abe7c0665047f05bb4d0cf28012767caeb8ba
Gochan57 <gochan@mail.ru>
2017-10-21 19:03:01 +0300

    Иконка университета

--- src/main/webapp/js/custom.js
+++ src/main/webapp/js/custom.js
@@ -1,26 +1,33 @@
+var universities = [[56.839563,60.6326293]]
+
 $(function () {
 
     function initMap() {
 
-        var location = new google.maps.LatLng(50.0875726, 14.4189987);
+        var location = new google.maps.LatLng(56.8546004, 60.638816);
 
         var mapCanvas = document.getElementById('map');
         var mapOptions = {
             center: location,
-            zoom: 16,
+            zoom: 12,
             panControl: false,
             scrollwheel: false,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         }
         var map = new google.maps.Map(mapCanvas, mapOptions);
 
-        var markerImage = 'marker.png';
+        universities.forEach(function(coords) {
+            var loc = new google.maps.LatLng(coords[0], coords[1]);
+            var marker = new google.maps.Marker({
+                position: loc,
+                map: map,
+                icon: './img/university.png'
+            });
 
-        var marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            icon: markerImage
-        });
+            marker.addListener('click', function () {
+                infowindow.open(map, marker);
+            });
+        })
 
         var contentString = '<div class="info-window">' +
             '<h3>Info Window Content</h3>' +
@@ -34,9 +41,7 @@ $(function () {
             maxWidth: 400
         });
 
-        marker.addListener('click', function () {
-            infowindow.open(map, marker);
-        });
+
 
         var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
 
